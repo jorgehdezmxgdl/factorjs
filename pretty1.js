5076e3fa382d17a155e407d71ddfd44ef62a9dbd
jorgehdezmxgdl <67309059+jorgehdezmxgdl@users.noreply.github.com>
2022-06-29 19:32:16 -0500

    Update pretty1.js

--- pretty1.js
+++ pretty1.js
@@ -1,14 +1,3 @@
-const esPrimo = numero => {
-	if (numero == 0 || numero == 1 || numero == 4) 
-        return false;
-	for (let x = 2; x < numero / 2; x++) {
-		if (numero % x == 0) {
-		    return false;
-		}
-	}
-	return true;
-}
-
 var Convert = Convert || {};
 
 Convert.base32toHex = function (data) {
